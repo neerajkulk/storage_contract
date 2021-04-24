db9476d36b3c68e85becc34535750ed1d30d780a
neerajkulk <nrjklk@gmail.com>
2021-04-23 21:13:15 -0600

    deploying contract

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { MNEMONIC, RINKEBY_URL } = require("../secrets");
+const { MNEMONIC, RINKEBY_URL } = require("./secrets");
 const { abi, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider({
@@ -13,13 +13,18 @@ const provider = new HDWalletProvider({
 const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log("Attempting to deploy from account: ", accounts[0]);
   const result = await new web3.eth.Contract(abi)
     .deploy({
       data: bytecode,
     })
     .send({ from: accounts[0], gas: "1000000" });
 
-  console.log("Contract deployed to", result.options.address);
+  return result;
 };
-deploy();
+
+deploy()
+  .then((result) => {
+    console.log("Succesfully deployed contract to:", result.options.address);
+  })
+  .catch((err) => console.log(err));
